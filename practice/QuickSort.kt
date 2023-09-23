d51975984ca0e38bdd81e1f7f5fe1ef62a43dbfb
Sutaek Oh <39640447+diskerr@users.noreply.github.com>
2023-09-24 03:07:49 +0900

    Create QuickSort.kt

---
+++ practice/QuickSort.kt
@@ -0,0 +1,43 @@
+fun main() {
+    repeat(100) {
+        val a = IntArray(100) { (-1000..1000).random() }
+        val o = a.clone()
+        qs(a)
+        if (!validate(a)) {
+            println("Error: ${o.joinToString()}")
+            println("Error: ${a.joinToString()}")
+            
+        }
+    }
+}
+
+fun validate(arr: IntArray): Boolean {
+    for (i in 1 until arr.size) {
+        if (arr[i - 1] > arr[i]) return false
+    }
+    
+    return true
+}
+
+fun qs(arr: IntArray, left: Int = 0, right: Int = arr.size - 1) {
+    if (right - left < 1) return
+    
+    val pivot = arr[(left + right) / 2]
+  
+    var l = left
+    var r = right
+
+    while (l <= r) {
+	    while (arr[l] < pivot) ++l
+    	while (arr[r] > pivot) --r
+    
+        if (l <= r) {
+            arr[l] = arr[r].also { arr[r] = arr[l] }
+            ++l
+            --r
+        }
+    }
+
+    qs(arr, left, r)
+    qs(arr, l, right)
+}
