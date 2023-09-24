cff704c692ecf7fc4c30034705913fecef269772
Sutaek Oh <39640447+diskerr@users.noreply.github.com>
2023-09-24 09:56:27 +0900

    Update 743. Network Delay Time.kt

--- leetcode/Medium/743. Network Delay Time.kt
+++ leetcode/Medium/743. Network Delay Time.kt
@@ -26,3 +26,39 @@ class Solution {
         return cost.max().takeUnless { it == Int.MAX_VALUE } ?: -1
     }
 }
+
+// pq 사용한 버전
+class Solution {
+    data class Node(val v: Int, var w: Int): Comparable<Node> {
+        override fun compareTo(other: Node): Int = w - other.w
+    }
+
+    fun networkDelayTime(times: Array<IntArray>, n: Int, k: Int): Int {
+        // key: u, value: Pair(v, w)
+        val adj: Map<Int, List<Pair<Int, Int>>> = times.groupBy({ it[0] }) { it[1] to it[2] }
+        val visited = BooleanArray(n + 1) { false }
+        val cost = IntArray(n + 1) { Int.MAX_VALUE }
+
+        cost[0] = 0
+        cost[k] = 0
+
+        val pq = PriorityQueue<Node>().apply { add(Node(k, 0)) }
+
+        while (pq.isNotEmpty()) {
+            val u = pq.remove().v
+
+            if (visited[u]) continue
+            visited[u] = true
+
+            adj[u]?.forEach { (v, w) ->
+                val c = cost[u] + w
+                if (c < cost[v]) {
+                    cost[v] = c
+                    pq.add(Node(v, c))
+                } 
+            }
+        }
+
+        return cost.max().takeUnless { it == Int.MAX_VALUE } ?: -1
+    }
+}
