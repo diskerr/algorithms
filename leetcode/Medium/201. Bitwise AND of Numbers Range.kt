6f9f94564d627845855cc0d28c49e3811a1c864e
Sutaek Oh <39640447+diskerr@users.noreply.github.com>
2024-01-27 14:34:17 +0900

    Update 201. Bitwise AND of Numbers Range.kt

--- leetcode/Medium/201. Bitwise AND of Numbers Range.kt
+++ leetcode/Medium/201. Bitwise AND of Numbers Range.kt
@@ -1,3 +1,4 @@
+// https://leetcode.com/problems/bitwise-and-of-numbers-range/solutions/4090679/java-2-line-solution-and-kotlin-1-line-solution-no-iteration-and-no-floating-point-math/
 class Solution {
     fun rangeBitwiseAnd(left: Int, right: Int): Int =
         (32 - (left xor right).countLeadingZeroBits()).let { left shr it shl it }
